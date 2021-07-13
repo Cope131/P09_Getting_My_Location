4a190da43391dacf2e8e7d8d3bbd25b6c86621b9
Daryl <darylgialolo@gmail.com>
2021-07-13 16:31:58 +0800

    1st commit

---
+++ app/src/main/java/com/myapplicationdev/android/p09gettingmylocation/MainActivity.java
@@ -0,0 +1,279 @@
+package com.myapplicationdev.android.p09gettingmylocation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnMapReadyCallback {
+
+    private final String DEBUG_TAG = MainActivity.class.getSimpleName();
+
+    private final int MY_LOCATION_REQUEST_CODE = 0;
+    private final int REQUEST_CODE = 101;
+    private final int REQUEST_CODE_2 = 102;
+
+    // Views
+    private Button checkRecordsBtn, getLocationUpdateBtn, removeLocationUpdateBtn;
+    private TextView lngTV, latTV;
+    private GoogleMap googleMap;
+
+    // Location
+    private Location lastLocation;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
+
+    // Map
+    private Marker lastLocationMarker;
+
+    // File Directory to Save Records
+    private String folderLocPath;
+    private File recordFile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        folderLocPath = getFilesDir().getAbsolutePath() + "/LocationRecords";
+        initViews();
+        initMap();
+        askPermission();
+    }
+
+    private void initViews() {
+        checkRecordsBtn = findViewById(R.id.check_records_button);
+        getLocationUpdateBtn = findViewById(R.id.get_location_update_button);
+        removeLocationUpdateBtn = findViewById(R.id.remove_location_update_button);
+        lngTV = findViewById(R.id.longitude_text_view);
+        latTV = findViewById(R.id.latitude_text_view);
+        checkRecordsBtn.setOnClickListener(this);
+        getLocationUpdateBtn.setOnClickListener(this);
+        removeLocationUpdateBtn.setOnClickListener(this);
+    }
+
+    // --- Click Button ---
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.check_records_button:
+                checkRecords();
+                break;
+            case R.id.get_location_update_button:
+                getLocationUpdate();
+                break;
+            case R.id.remove_location_update_button:
+                removeLocationUpdates();
+        }
+    }
+
+    private void checkRecords() {
+        Intent intent = new Intent(this, RecordsActivity.class);
+        intent.putExtra("folderPath", folderLocPath);
+        intent.putExtra("fileName", "records.txt");
+        startActivity(intent);
+    }
+
+    private void removeLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback);
+        Toast.makeText(this, "Location Update Disabled", Toast.LENGTH_SHORT).show();
+    }
+
+    private void getLocationUpdate() {
+        initLocationComp();
+        Toast.makeText(this, "Location Update Enabled", Toast.LENGTH_SHORT).show();
+    }
+
+    private void changeLocationRequestProps() {
+        fusedLocationClient.removeLocationUpdates(locationCallback);
+        locationRequest.setInterval(30 * 1000).setSmallestDisplacement(500 * 10);
+    }
+
+    // --- Location Request ---
+    private void initLocationComp() {
+        // Connect to google play location services
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        // Location Request Settings
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10000)
+                .setFastestInterval(5000)
+                .setSmallestDisplacement(100);
+        // Listener for Location Updates
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult != null) {
+                    lastLocation = locationResult.getLastLocation();
+                    double lat = lastLocation.getLatitude();
+                    double lng = lastLocation.getLongitude();
+                    latTV.setText(String.valueOf(lat));
+                    lngTV.setText(String.valueOf(lng));
+                    Log.d(DEBUG_TAG, "Lat: " + lat + " Lang: " + lng);
+                    showMarkerLastLocation();
+                    saveToDir();
+                } else {
+                    Toast.makeText(MainActivity.this, "Location Update is Disabled", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+        // Start Requesting for Location Updates
+        if (checkPermission()) {
+            Log.d(DEBUG_TAG, "Check Permission Result: " + checkPermission());
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+        }
+    }
+
+    // --- Permissions ---
+    private void askPermission() {
+        if (!checkPermission()) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE);
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[] {Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_CODE_2);
+        }
+    }
+
+    private boolean checkPermission() {
+        int permissionCheck_Coarse
+                = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        int permissionCheck_Fine
+                = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionCheck_Coarse == PermissionChecker.PERMISSION_GRANTED ||
+                permissionCheck_Fine == PermissionChecker.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE || requestCode == REQUEST_CODE_2) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initLocationComp();
+            }  else {
+                Toast.makeText(MainActivity.this, "Permission not granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // --- Map ---
+    private void initMap() {
+        SupportMapFragment mapFragment
+                = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        if (googleMap != null) {
+            // Zoom in to Sg
+            LatLng sg = new LatLng(1.3558701658116, 103.86277464840944);
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sg, 11));
+            // Enable Features
+            UiSettings uiSettings = googleMap.getUiSettings();
+            uiSettings.setCompassEnabled(true);
+            uiSettings.setZoomControlsEnabled(true);
+            uiSettings.setZoomGesturesEnabled(true);
+            uiSettings.setMyLocationButtonEnabled(true);
+
+            if (checkPermission()) {
+                googleMap.setMyLocationEnabled(true);
+            } else {
+                askPermission();
+            }
+        }
+    }
+
+    private void showMarkerLastLocation() {
+        if (lastLocationMarker != null) {
+            lastLocationMarker.remove();
+        }
+        if (googleMap != null) {
+            // Add Marker
+            LatLng position = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+            MarkerOptions markerOptions = new MarkerOptions()
+                    .position(position)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+            lastLocationMarker = googleMap.addMarker(markerOptions);
+            // Zoom into Marker
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 11));
+        }
+    }
+
+    // --- File Record ---
+    private void saveToDir() {
+        if (lastLocation != null) {
+            // Create Directory if it doesnt exist
+            createDir();
+            // Write Location Record
+            writeRecord();
+        }
+    }
+
+    private void createDir() {
+        Log.d(DEBUG_TAG, folderLocPath);
+        File folder = new File(folderLocPath);
+        if (!folder.exists()) {
+            boolean isCreated = folder.mkdir();
+            Log.d(DEBUG_TAG, isCreated ? "Folder Created" : "Folder Not Created");
+        }
+    }
+
+    private void writeRecord() {
+        recordFile = new File(folderLocPath, "records.txt");
+        Log.d(DEBUG_TAG, folderLocPath + recordFile.getAbsolutePath());
+        try {
+            FileWriter writer = new FileWriter(recordFile, true);
+            String location =lastLocation.getLatitude() + ", " + lastLocation.getLongitude();
+            writer.write(location + "\n");
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            Toast.makeText(this, "Failed to write!", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/myapplicationdev/android/p09gettingmylocation/RecordsActivity.java
@@ -0,0 +1,87 @@
+package com.myapplicationdev.android.p09gettingmylocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class RecordsActivity extends AppCompatActivity {
+
+    private final String DEBUG_TAG = RecordsActivity.class.getSimpleName();
+    private File recordFile;
+
+    // List View Components
+    private ListView lv;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> locations;
+
+    // Views
+    private TextView recordsTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_records);
+        initViews();
+        initLVComp();
+        getFilePath();
+        read();
+    }
+
+    private void initViews() {
+        recordsTV = findViewById(R.id.number_of_records_text_view);
+    }
+
+    private void initLVComp() {
+        lv = findViewById(R.id.list_view);
+        locations = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(RecordsActivity.this, android.R.layout.simple_list_item_1, locations);
+        lv.setAdapter(arrayAdapter);
+    }
+
+    private void getFilePath() {
+        String fileName = getIntent().getStringExtra("fileName");
+        String folderPath = getIntent().getStringExtra("folderPath");
+        recordFile = new File(folderPath, fileName);
+    }
+
+    private void read() {
+        if (recordFile != null && recordFile.exists()) {
+            StringBuilder sb = new StringBuilder();
+            try {
+                FileReader reader = new FileReader(recordFile);
+                BufferedReader br = new BufferedReader(reader);
+                locations.clear();
+                String line = br.readLine();
+                while (line != null) {
+                    sb.append(line + "\n");
+                    if (line != null) {
+                        locations.add(line);
+                    }
+                    line = br.readLine();
+                }
+                br.close();
+                reader.close();
+                Log.d(DEBUG_TAG, "records.txt Content: \n" + sb.toString());
+                Log.d(DEBUG_TAG, locations.toString());
+
+                arrayAdapter.notifyDataSetChanged();
+                recordsTV.setText(locations.size() + "");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
